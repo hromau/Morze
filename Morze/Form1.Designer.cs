2f5aa43533aba4cc3fde17d8b8558e03e15f9c00
Громов Сергей <by.skleroz@ya.ru>
2017-09-07 20:46:43 +0300

    cmd commit

---
+++ Morze/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Morze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerOfThread = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(225, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 108);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Button1_MouseUp);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // timerOfThread
+            // 
+            this.timerOfThread.Tick += new System.EventHandler(this.TimerOfThread_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 240);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.Timer timerOfThread;
+    }
+}
+
---
+++ Morze/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Morze
+{
+    public partial class Form1 : Form
+    {
+        public System.Windows.Forms.Timer generalTimerObj = new System.Windows.Forms.Timer();
+        public System.Windows.Forms.Timer timerThreadObj = new System.Windows.Forms.Timer();
+        //public Thread StartThreadTimer = new Thread(ThreadTimerStart(timerThreadObj));//передать параметры нужно как-то. иначе нечего запускать ведь
+        public Thread StopThreadTimer = new Thread(ThreadTimerStop);
+        public Thread StartGeneralTimer = new Thread(GeneralTimerStart);
+        public Thread StopGeneralTimer= new Thread(GeneralTimerStop);
+        public int lengthOfThreadTimer = 0;
+        string StringOfSignal { get; set; } = "";
+        public Dictionary<string, char> ABC = new Dictionary<string, char>
+        {
+            { "01",'а' },
+            { "1000",'б' },
+            { "011",'в' },
+            { "110",'г' },
+            { "100",'д' },
+            { "0",'е' },
+            { "0001",'ж' },
+            { "1100",'з' },
+            { "00",'и' },
+            { "0111",'й' },
+            { "101",'к' },
+            { "0100",'л' },
+            { "11",'м' },
+            { "10",'н' },
+            { "111",'о' },
+            { "0110",'п' },
+            { "010",'р' },
+            { "000",'с' },
+            { "1",'т' },
+            { "001",'у' },
+            { "0010",'ф' },
+            { "0000",'х' },
+            { "1010",'ц' },
+            { "1110",'ч' },
+            { "1111",'ш' },
+            { "1101",'щ' },
+            { "1001",'ь' },
+            { "1011",'ы' },
+            { "00100",'э' },
+            { "0011",'ю' },
+            { "0101",'я' }
+        };
+        //public Task taskOfCountTime = new Task();
+        public string direction = "";
+        public Thread threadOfCountTime = new Thread(CountTime);
+        public int lengthOfTime = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.BackColor = Color.Yellow;
+            generalTimerObj = timer1;
+            timerThreadObj = timerOfThread;
+        }
+        public static void CountTime(object timerObj)
+        {
+            System.Windows.Forms.Timer timer = (System.Windows.Forms.Timer)timerObj;
+            
+        }
+
+        #region ThreadofTimers
+        public static void ThreadTimerStart(object timerofThread)
+        {
+            var timer = (System.Windows.Forms.Timer)timerofThread;
+            timer.Start();
+        }
+        public static void ThreadTimerStop(object timerofThread)
+        {
+            var timer = (System.Windows.Forms.Timer)timerofThread;
+            timer.Stop();
+        }
+        public static void GeneralTimerStart(object generalTimer)
+        {
+            var timer = (System.Windows.Forms.Timer)generalTimer;
+            timer.Start();
+        }
+        public static void GeneralTimerStop(object generalTimer)
+        {
+            var timer = (System.Windows.Forms.Timer)generalTimer;
+            timer.Stop();
+        }
+        #endregion
+
+        /// <summary>
+        /// Метод который считывает окончание ввода, преобразует сигналы в букву, обнуляет данные
+        /// </summary>
+        /// <param name="timer1">Таймер</param>
+        /// <param name="ofSignal">Строка сигналов: нулей и единиц</param>
+        /// <param name="ABC">Словарь символов</param>
+        /// <returns></returns>
+        public static string WaitDirectionAndOutput(ref System.Windows.Forms.Timer timer1,string ofSignal,Dictionary<string,char> ABC)
+        {
+            int tempLengthTime = 0;
+            timer1.Start();
+            string result = "";
+            while (true)
+            {
+                tempLengthTime += timer1.Interval;
+                if (tempLengthTime > 3000)
+                {
+                    timer1.Stop();
+                    foreach (var item in ABC)
+                    {
+                        if (item.Key == ofSignal)
+                        {
+                            result += item.Value;
+                        }
+                    }
+                    break;
+                }
+            }
+            return result;
+        }
+
+
+
+        private void Button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            StopThreadTimer.Start();
+            StartGeneralTimer.Start();
+            button1.BackColor = Color.Green;
+        }
+
+        private void Button1_MouseUp(object sender, MouseEventArgs e)
+        { 
+            StopGeneralTimer.Start();
+            button1.BackColor = Color.Red;
+            if (lengthOfTime<=1000)
+            {
+                //короткий сигнал 
+                StringOfSignal += "0";
+            }
+            if (lengthOfTime>1000)
+            {
+                //длинынй сигнал
+                StringOfSignal += "1";
+            }
+            //метод для разборки слова и для нового начала
+            StartThreadTimer.Start();
+            //richTextBox1.Text+= WaitDirectionAndOutput(ref timer1, StringOfSignal,ABC);
+            string result = "";
+            while (true)
+            {
+                if (lengthOfThreadTimer > 3000)
+                {
+                    StopThreadTimer.Start();
+                    foreach (var item in ABC)
+                    {
+                        if (item.Key == StringOfSignal)
+                        {
+                            result += item.Value;
+                        }
+                    }
+                    break;
+                }
+            }
+            lengthOfThreadTimer = 0;
+            lengthOfTime = 0;
+            button1.BackColor = Color.Yellow;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            lengthOfTime += 100;
+        }
+
+        private void TimerOfThread_Tick(object sender, EventArgs e)
+        {
+            lengthOfThreadTimer += 100;
+        }
+    }
+}
